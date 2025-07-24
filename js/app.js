1276124666b119c9d9b1187bfd5201a8494b9f30
Matheus de Souza <matheusgds3011@gmail.com>
2025-07-24 18:28:57 -0300

    Exercício de repetição para fixação

--- js/app.js
+++ js/app.js
@@ -15,7 +15,24 @@ function alterarStatus(id){
         imagem.classList.add("dashboard__item__img--rented");
     }
 }
+
+function alterarStatus(id){
+    let jogo = document.getElementById(`game-${id}`);
+    let imagem = jogo.querySelector(".dashboard__item__img");
+    let botao = jogo.querySelector(".dashboard__item__button");
+
+    if(botao.classList.contains("dashboard__item__button--return")){
+        botao.classList.remove("dashboard__item__button--return");
+        botao.textContent = "Alugar";
+        imagem.classList.remove("dashboard__item__img--rented");
+    } else {
+        botao.classList.add("dashboard__item__button--return");
+        botao.textContent = "Devolver";
+        imagem.classList.add("dashboard__item__img--rented");
+    }
+}
 */
+
 function alterarStatus(id){
     let jogo = document.getElementById(`game-${id}`);
     let imagem = jogo.querySelector(".dashboard__item__img");
